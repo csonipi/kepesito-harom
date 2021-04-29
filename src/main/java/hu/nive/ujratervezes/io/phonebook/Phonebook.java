bb6fc57a57279d9f4ffccac20e8739349e482a70
csonipi <csoni.pi@gmail.com>
2021-04-29 10:46:48 +0200

    all green

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,22 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output){
+        if(contacts == null || output == null){
+            throw new IllegalArgumentException("Null object");
+        }
+        try{
+            FileWriter fileWriter = new FileWriter(output);
+            for (Map.Entry entry:contacts.entrySet()) {
+                fileWriter.write((String) entry.getKey() + ": " + entry.getValue() + "\n");
+            }
+            fileWriter.close();
+        }catch(IOException e){
+
+        }
+    }
+}
